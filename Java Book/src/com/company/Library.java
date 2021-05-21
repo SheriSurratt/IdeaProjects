8309e5cb3954c21d4adb0517acc171e395ed60f7
Sheri Surratt <sherisurratt@gmail.com>
2021-05-21 10:54:42 -0400

    First Commit

---
+++ Java Book/src/com/company/Library.java
@@ -0,0 +1,21 @@
+package com.company;
+
+public class Library {
+    public Library(String bookTitle ) {
+    }
+
+    public void addBook(Book book) {
+    }
+
+    public void printAddress() {
+    }
+
+    public void borrowBook(String the_lord_of_the_rings) {
+    }
+
+    public void printAvailableBooks() {
+    }
+
+    public void returnBook(String the_lord_of_the_rings) {
+    }
+}   
